cd2d17e1312323fed47dc4ebdc457720098d6f22
Dilwar96 <157256888+Dilwar96@users.noreply.github.com>
2024-08-26 20:25:09 +0200

    add the Slider to Hero Component

--- src/components/start/Hero.jsx
+++ src/components/start/Hero.jsx
@@ -1,37 +1,60 @@
 import hero1 from "../../assets/salon1.jpg";
 import hero2 from "../../assets/salon2.jpg";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const carouselImages = [hero1, hero2];
 
 const Hero = () => {
-  return (
-    <div className="grid lg:grid-cols-2 gap-24 items-center bg-black py-5 ">
-      <div className="ml-8 lg:ml-40">
-        <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl text-yellow-500">
-          Willkommen im unserem Friseursalon
-        </h1>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 750,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+  };
 
-        <p className="mt-8 max-w-xl text-lg leading-8 text-yellow-500">
+  return (
+    <div className="relative h-[500px] overflow-hidden">
+      {" "}
+      <Slider {...settings}>
+        <div>
+          <img
+            src={hero1}
+            alt="Innenansicht des Barbier-Salons"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src={hero2}
+            alt="Vorher-Nachher-Frisur"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="/path/to/image3.jpg"
+            alt="Detailansicht der Friseurwerkzeuge"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </Slider>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="text-yellow-500 text-4xl font-bold mb-2">
+          Willkommen bei unserem Friseursalon
+        </h1>
+        <p className="text-yellow-500 text-xl mb-4">
           Dein perfekter Look, unser Handwerk
         </p>
-        <div className="mt-10">
-          <Link to="/services" className="btn btn-warning">
-            UNSERE LEISTUNGEN
-          </Link>
-        </div>
-      </div>
-      <div className="hidden  h-[28rem] lg:carousel carousel-center   p-4 space-x-4 bg-accent-content rounded-box">
-        {carouselImages.map((image, index) => {
-          return (
-            <div key={image} className="carousel-item">
-              <img
-                src={image}
-                className="rounded-box h-full w-80  object-cover"
-              />
-            </div>
-          );
-        })}
+        <Link to="/services" className="btn btn-warning">
+          UNSERE LEISTUNGEN
+        </Link>
       </div>
     </div>
   );
